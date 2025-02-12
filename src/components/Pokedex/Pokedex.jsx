575a7dd3dc41896ba161011e62d6d7817e583dd8
BharathJP-72 <bharathjp72@gmail.com>
2025-02-12 18:30:03 +0530

    added debouncing

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -1,14 +1,18 @@
 import './Pokedex.css';
 import Search from '../Search/Search';
-import React from 'react'
+import React, { useState } from 'react'
 import PokemonList from '../PokemonList/PokemonList';
+import PokemonDetails from '../PokemonDetails/PokemonDetails'
 
 function Pokedex() {
+
+    const [searchTerm, setSearchTerm] = useState('')
+
     return(
         <div className='pokedex-wrapper'>
             <h1>Pokedex</h1>
-            <Search /> 
-            <PokemonList />
+            <Search updateSearchTerm = {setSearchTerm} /> 
+            {searchTerm ? <PokemonDetails pokemonName={searchTerm}/> : <PokemonList />}
         </div>
     )
 }
